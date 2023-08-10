3f156c86bf7d05effdfe3ae35063698d0f66d7b6
shayan memarzade <shayan.memarzade@live.com>
2023-08-11 00:31:04 +0300

    fix: improve remove text

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -106,7 +106,7 @@ const Editor = () => {
                                 <button type="button"
                                   className=" text-white bg-red-700 hovebg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 h-10"
                                   onClick={() => removeBlock(index)}>
-                                  REmove
+                                  Remove
                                 </button>
                               </div>
                             </div>
